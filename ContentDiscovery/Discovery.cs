797dc469c1ebf0a43ef749ef6141c8b2ec85fb61
xPumaa <35280763+xPumaa@users.noreply.github.com>
2022-04-08 23:14:10 +0200

    Message only in duty roulettes

--- ContentDiscovery/Discovery.cs
+++ ContentDiscovery/Discovery.cs
@@ -13,6 +13,8 @@ public sealed class Discovery
     private readonly ChatGui _chatGui;
     private ExcelSheet<ContentFinderCondition>? _contentFinderConditionsSheet;
 
+    private bool dutyRoulette = false;
+
     public Discovery(ClientState client, DataManager data, ChatGui chat)
     {
         _clientState = client;
@@ -24,6 +26,7 @@ public sealed class Discovery
 
     public void Initialize()
     {
+        _clientState.CfPop += CfPop;
         _clientState.TerritoryChanged += OnTerritoryChanged;
         _contentFinderConditionsSheet = _dataManager.GameData.GetExcelSheet<ContentFinderCondition>();
     }
@@ -33,7 +36,7 @@ public sealed class Discovery
         if(_contentFinderConditionsSheet != null)
         {
             var content = _contentFinderConditionsSheet.FirstOrDefault(t => t.TerritoryType.Row == _clientState.TerritoryType);
-            if (content != null)
+            if (content != null && dutyRoulette)
             {
                 _chatGui.Print($"Entering: {content.Name}");
             }
@@ -48,8 +51,21 @@ public sealed class Discovery
         }
     }
 
+    private void CfPop(object? sender, ContentFinderCondition e)
+    {
+        dutyRoulette = false;
+    
+        if (_contentFinderConditionsSheet != null)
+        {
+            var content = _contentFinderConditionsSheet.FirstOrDefault(t => t.TerritoryType.Row == e.TerritoryType.Row);
+    
+            dutyRoulette = content != null && String.IsNullOrEmpty(content.Name);
+        }
+    }
+
     public void Dispose()
     {
         _clientState.TerritoryChanged -= OnTerritoryChanged;
+        _clientState.CfPop -= CfPop;
     }
 }
